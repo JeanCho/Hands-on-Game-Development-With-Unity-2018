186ce1a6b8c464e53b0d71ab8314a76e70805a41
JeanCho <nicetoto97@naver.com>
2019-09-09 19:01:25 +0900

    Completed camera and Charcter controls

---
+++ RogueSmashRepo/RogueSmash/Assets/Prototype/Scripts/CharacterControls.cs
@@ -0,0 +1,55 @@
+﻿
+using UnityEngine;
+
+public class CharacterControls : MonoBehaviour
+{
+    /// The actor is the game object in the world
+    /// representing this character/ entity
+    // Start is called before the first frame update
+    
+    [SerializeField] private GameObject actor;
+
+
+    ///Movement speed is linearly multiplied by this value
+
+    [SerializeField] private float moveSpeedModifier = 3;
+
+
+    private Vector3 mousePosition;
+    private Vector3 lookDirection;
+
+    public void FixedUpdate() 
+    {
+        HandleInput();
+        
+    }
+
+    private void HandleInput()
+    {
+        Quaternion lookRotation = GetMouseInput();
+        actor.transform.rotation = lookRotation;
+
+        Vector3 moveDirection = GetInput();
+        actor.transform.Translate(moveDirection * Time.deltaTime * moveSpeedModifier, Space.World);
+
+
+    }
+
+
+    private Vector3 GetInput()
+    {
+        Vector3 input = Vector3.zero;
+        input.x = Input.GetAxis("Horizontal");
+        input.z = Input.GetAxis("Vertical");
+        return input;
+    }
+
+    private Quaternion GetMouseInput()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 relativeMousePos = new Vector2(mousePos.x - Screen.width / 2, mousePos.y - Screen.height / 2);
+        float angle = Mathf.Atan2(relativeMousePos.y, relativeMousePos.x) * Mathf.Rad2Deg * -1;
+        Quaternion rot = Quaternion.AngleAxis(angle, Vector3.up);
+        return rot;
+    }
+}
